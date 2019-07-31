afb2056396c96bdfaa1765ba79732d184a612052
tung <tungtrinh17@gmail.com>
2019-07-31 18:24:58 +0700

    t

--- src/components/DetailPage/FoodDetail.js
+++ src/components/DetailPage/FoodDetail.js
@@ -24,8 +24,7 @@ class FoodDetail extends React.Component
 
     render()
     {
-        if(this.props.Data.foods == undefined || this.props.Data.foods == 'undefined'
-        || this.props.Data.foods == '')
+        if(this.props.Data.foods)
         {
             return(
                 <Loadding/>
